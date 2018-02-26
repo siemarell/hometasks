deeaa16152f1297e1e9815fdf6d56262227277ce
siem <siemarell@gmail.com>
2018-02-26 17:53:35 +0300

    mempool generator

--- src/main/scala/transaction/BlockchainDevelopersMempool.scala
+++ src/main/scala/transaction/BlockchainDevelopersMempool.scala
@@ -5,41 +5,41 @@ import scorex.core.transaction.MemoryPool
 
 import scala.util.{Failure, Success, Try}
 
-class BlockchainDevelopersMempool(txs: Map[ModifierId, BlockchainDevelopersTransaction]) extends MemoryPool[BlockchainDevelopersTransaction, BlockchainDevelopersMempool] {
+class BlockchainDevelopersMempool(transactionsMap: Map[ModifierId, BlockchainDevelopersTransaction]) extends MemoryPool[BlockchainDevelopersTransaction, BlockchainDevelopersMempool] {
 
-  override def put(tx: BlockchainDevelopersTransaction): Try[BlockchainDevelopersMempool] = Try(
-    if (this.contains(tx.id)){
-      throw new Exception(s"Mempool already contains transanction with id ${tx.id}")
-    }else {
-      val newTxs = this.txs + (tx.id -> tx)
-      new BlockchainDevelopersMempool(newTxs)
+  override def put(tx: BlockchainDevelopersTransaction): Try[BlockchainDevelopersMempool] = {
+    if (contains(tx.id)) {
+      Failure(new Exception(s"Mempool already contains transanction with id ${tx.id}"))
+    } else {
+      val newTxs = transactionsMap + (tx.id -> tx)
+      Success(new BlockchainDevelopersMempool(newTxs))
     }
-  )
+  }
 
-  override def put(txs: Iterable[BlockchainDevelopersTransaction]): Try[BlockchainDevelopersMempool] = Try(
+  override def put(txs: Iterable[BlockchainDevelopersTransaction]): Try[BlockchainDevelopersMempool] = Try(putWithoutCheck(txs))
+
+
+  override def putWithoutCheck(txs: Iterable[BlockchainDevelopersTransaction]): BlockchainDevelopersMempool =
     txs.foldLeft(this)((inter,tx)=> inter.put(tx) match {
       case Success(mempool) => mempool
       case Failure(e) => throw new Exception(e)
     })
-  )
 
-  override def putWithoutCheck(txs: Iterable[BlockchainDevelopersTransaction]): BlockchainDevelopersMempool = ???
-
-  override def remove(tx: BlockchainDevelopersTransaction): BlockchainDevelopersMempool = new BlockchainDevelopersMempool(this.txs - tx.id)
+  override def remove(tx: BlockchainDevelopersTransaction): BlockchainDevelopersMempool = new BlockchainDevelopersMempool(transactionsMap - tx.id)
 
   override def filter(condition: BlockchainDevelopersTransaction => Boolean): BlockchainDevelopersMempool = new BlockchainDevelopersMempool(
-    this.txs.filter(kv => condition(kv._2))
+    this.transactionsMap.filter(kv => condition(kv._2))
   )
 
-  override def getById(id: ModifierId): Option[BlockchainDevelopersTransaction] = this.txs.get(id)
+  override def getById(id: ModifierId): Option[BlockchainDevelopersTransaction] = transactionsMap.get(id)
 
-  override def contains(id: ModifierId): Boolean = this.txs.contains(id)
+  override def contains(id: ModifierId): Boolean = transactionsMap.contains(id)
 
-  override def getAll(ids: Seq[ModifierId]): Seq[BlockchainDevelopersTransaction] = this.txs.filter(kv=> ids.contains(kv._1)).values.toIndexedSeq
+  override def getAll(ids: Seq[ModifierId]): Seq[BlockchainDevelopersTransaction] = transactionsMap.filter(kv=> ids.contains(kv._1)).values.toIndexedSeq
 
-  override def size: Int = txs.size
+  override def size: Int = transactionsMap.size
 
-  override def take(limit: Int): Iterable[BlockchainDevelopersTransaction] = txs.take(limit).values
+  override def take(limit: Int): Iterable[BlockchainDevelopersTransaction] = transactionsMap.take(limit).values
 
   override type NVCT = BlockchainDevelopersMempool
 }
--- src/test/scala/transaction/BDMempoolTest.scala
+++ src/test/scala/transaction/BDMempoolTest.scala
@@ -3,6 +3,7 @@ package transaction
 import org.scalacheck.Gen
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
+import scorex.crypto.hash.{Digest32, Sha256}
 import scorex.testkit.generators.CoreGenerators
 import scorex.testkit.properties.mempool.MempoolTransactionsTest
 
@@ -11,10 +12,26 @@ class BDMempoolTest extends PropSpec
   with GeneratorDrivenPropertyChecks
   with Matchers
   with CoreGenerators
-  with MempoolTransactionsTest[Sha256PreimageProposition, BlockchainDevelopersTransaction, BlockchainDevelopersMempool]
-{
+  with MempoolTransactionsTest[Sha256PreimageProposition, BlockchainDevelopersTransaction, BlockchainDevelopersMempool] {
 
-  override val memPool: BlockchainDevelopersMempool = ???
-  override val transactionGenerator: Gen[BlockchainDevelopersTransaction] = ???
+  override val memPool: BlockchainDevelopersMempool = new BlockchainDevelopersMempool(Map())
+  override val transactionGenerator: Gen[BlockchainDevelopersTransaction] = for {
+    nInputs <- smallInt.filter(_ > 0)
+    inputs: IndexedSeq[OutputId] <- Gen.listOfN(nInputs, genBytesList(32))
+    nOutputs <- smallInt.filter(_ > 0)
+    sigs: IndexedSeq[Sha256PreimageProof] <- Gen.listOfN(nOutputs, genBytesList(32))
+    txoVals: IndexedSeq[Value] <- Gen.listOfN(nOutputs, positiveLongGen)
+  } yield BlockchainDevelopersTransaction(inputs,
+    sigs.zip(txoVals).toIndexedSeq.map { case (sig, txoVal) =>
+      (Sha256PreimageProposition(Digest32 !@@ Sha256(sig)), Value @@ txoVal)
+    },
+    sigs)
 
+  property("Should add new TX"){
+    forAll(transactionGenerator){ tx =>
+      val newPool = memPool.put(tx)
+      newPool.isSuccess shouldBe true
+    }
+  }
 }
+
