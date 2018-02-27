df0d3bfd9d54fcb07b214c37c33ed85052a05063
siem <siemarell@gmail.com>
2018-02-27 12:01:14 +0300

    mempool tests

--- src/test/scala/transaction/BDMempoolTest.scala
+++ src/test/scala/transaction/BDMempoolTest.scala
@@ -17,20 +17,43 @@ class BDMempoolTest extends PropSpec
   override val memPool: BlockchainDevelopersMempool = new BlockchainDevelopersMempool(Map())
   override val transactionGenerator: Gen[BlockchainDevelopersTransaction] = for {
     nInputs <- smallInt.filter(_ > 0)
-    inputs: IndexedSeq[OutputId] <- Gen.listOfN(nInputs, genBytesList(32))
+    inputs <- Gen.listOfN(nInputs, genBytesList(32).map(OutputId @@ _))
     nOutputs <- smallInt.filter(_ > 0)
-    sigs: IndexedSeq[Sha256PreimageProof] <- Gen.listOfN(nOutputs, genBytesList(32))
-    txoVals: IndexedSeq[Value] <- Gen.listOfN(nOutputs, positiveLongGen)
-  } yield BlockchainDevelopersTransaction(inputs,
-    sigs.zip(txoVals).toIndexedSeq.map { case (sig, txoVal) =>
-      (Sha256PreimageProposition(Digest32 !@@ Sha256(sig)), Value @@ txoVal)
-    },
-    sigs)
+    digests <- Gen.listOfN(nOutputs, genBytesList(32))
+    txoVals <- Gen.listOfN(nOutputs, positiveLongGen)
+    signatures: IndexedSeq[Sha256PreimageProof] = digests.toIndexedSeq.map(item => Sha256PreimageProof(Digest32Preimage @@ item))
+    outputs: IndexedSeq[(Sha256PreimageProposition, Value)] = digests.zip(txoVals).toIndexedSeq.map{ case (digest, txoVal) => {
+      (Sha256PreimageProposition(Sha256.hash(digest)),Value @@ txoVal)
+    }}
+  } yield BlockchainDevelopersTransaction(inputs.toIndexedSeq,outputs,signatures)
 
   property("Should add new TX"){
     forAll(transactionGenerator){ tx =>
       val newPool = memPool.put(tx)
       newPool.isSuccess shouldBe true
+    }
+  }
+
+  property("Shouldn't add same TX"){
+    forAll(transactionGenerator){ tx =>
+      var newPool = memPool.put(tx)
+      newPool = newPool.get.put(tx)
+      newPool.isSuccess shouldBe false
+    }
+  }
+
+  property("Should find previously added TX"){
+    forAll(transactionGenerator){ tx =>
+      var newPool = memPool.put(tx)
+      newPool.get.contains(tx.id) shouldBe true
+    }
+  }
+  property("Should serialize and deserialize TX"){
+    forAll(transactionGenerator){ tx =>
+      val serialized = tx.bytes
+      val restored = tx.serializer.parseBytes(serialized)
+      val restoredBytes = restored.get.bytes
+      tx.bytes shouldEqual restoredBytes
     }
   }
 }
